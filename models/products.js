5997a50735d7408d6f37dc1aa4663869c9c271db
Ramitha Guruge <ramithaguruge@gmail.com>
2025-07-10 09:42:42 +0530

    create product model

---
+++ models/products.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+    productID : {
+        type : String,
+        unique : true
+
+    },
+
+    productName : {
+        type : String,
+        required : true
+    },
+
+    altNames : [
+        {
+            type : string
+        }
+    ],
+
+    images : [
+        {
+            type : string
+        }
+    ],
+
+    price : {
+        type : Number,
+        required : true
+    },
+
+    lastPrice : {
+        type : string,
+        required : true
+    },
+
+    description : {
+        type : string,
+        required : true
+    }
+
+})
+
+
