bf4dca6b796d87b2a7090a324a32e7493ec7353c
Ramitha Guruge <ramithaguruge@gmail.com>
2025-07-10 10:35:45 +0530

    Add admin validation for product creation in productController.js

--- controller/productController.js
+++ controller/productController.js
@@ -1,10 +1,38 @@
 import express from 'express'
-import Product from '../models/products'
+import Product from '../models/products.js'
 import mongoose from 'mongoose'
+import { isAdmin } from './userController.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
 export function createProduct (req,res){
-    
+
+    if (!isAdmin(req)){
+        res.json({
+            message : "Please login as Administrator to add product"
+        })
+        return
+    }
+
+    const newProdcutData = req.body;
+
+    const product = new Product(newProdcutData)
+
+    product.save().then(()=>{
+        res.json({
+            message : "product Successfully Added.."
+        })
+    }).catch((error)=>{
+        res.json({
+            message : error
+        })
+    })
 }
+
+export function getProduct(req,res){
+    Product.find({}).then((Product)=>{
+        res.json(Product)
+    })
+}
+
 
