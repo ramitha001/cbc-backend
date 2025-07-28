1a0259e03bf58f60ca8448a11530894652a72bcf
Ramitha Guruge <ramithaguruge@gmail.com>
2025-07-28 14:01:48 +0530

    Add DELETE endpoint for product deletion in productRouter.js

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createProduct, getProduct } from '../controller/productController.js';
+import { createProduct, deleteProduct, getProduct } from '../controller/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.post("/",createProduct)
 
 productRouter.get("/",getProduct)
+
+productRouter.delete("/:productId",deleteProduct)
 
 export default productRouter;
